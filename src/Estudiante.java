490869a83949f6c0c6757eafc5b576f6b79c53ee
HugoR96 <162131124+HugoR96@users.noreply.github.com>
2025-04-11 09:12:25 -0400

    Add files via upload

---
+++ src/Estudiante.java
@@ -0,0 +1,28 @@
+public class Estudiante extends Persona {
+        private String codeSis;
+        public Estudiante(String name, String firstLastName, String secondLastName, Date birthDate, boolean complementCi, String numberCi) {
+            super(name, firstLastName, secondLastName, birthDate, complementCi, numberCi);
+        }
+
+        public Estudiante() {
+            super();
+        }
+
+        public void addStudent(Scanner scanner) {
+            super.addPerson(scanner);
+
+            System.out.print("Código SIS: ");
+            this.codeSis = scanner.nextLine();
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "\n| CÓDIGO SIS |: " + codeSis + "\n";
+        }
+
+        public String sayCodigoSis() {
+            return codeSis;
+        }
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,54 @@
+import java.util.*;
 public class Main {
-    public static void main(String[] args) {
-       // Ejercicio9 ejercicio9 = new Ejercicio9();
-       // ejercicio9.ejecutar();
-       // Ejercicio10 ejercicio10 = new Ejercicio10();
-       // ejercicio10.execute();
-        Ejercicio11 ejercicio11 = new Ejercicio11();
-        ejercicio11.execute();
+            private Estudiante[] students;
+            private int studentsCounter;
+
+            public Main() {
+                this.students = new Estudiante[18];
+                this.studentsCounter = 0;
+            }
+
+            public void addStudents(Estudiante student) {
+                if (studentsCounter < 18) {
+                    students[studentsCounter] = student;
+                    studentsCounter++;
+                } else {
+                    System.out.println(" ------------------------------------");
+                    System.out.println(" | TODOS LOS ESTUDIANTE REGISTRADOS |");
+                    System.out.println(" ------------------------------------");
+                }
+            }
+
+            public void printStudents() {
+                System.out.println("\n | --->  L I S T A  D E  E S T U D I A N T E S  O R D E N A D O S  <--- |");
+                for (int i = 0; i < studentsCounter; i++) {
+                    System.out.println(students[i]);
+                }
+            }
+
+            public void sortStudentsByAge() {
+                students = Arrays.stream(students, 0, studentsCounter)
+                        .sorted(Comparator.comparingInt(Estudiante::calculateAge))
+                        .toArray(Estudiante[]::new);
+            }
+
+            public static void main(String[] args) {
+                Main newMain = new Main();
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.println("---------------------------------------");
+                System.out.println("| REGISTRO DE ESTUDIANTES DE LA CLASE |");
+                System.out.println("---------------------------------------");
+
+                int numStudents = 18;
+                for (int i = 0; i < numStudents; i++) {
+                    System.out.println("\n| ESTUDIANTE [" + (i + 1) + "] |");
+                    Estudiante student = new Estudiante();
+                    student.addStudent(scanner);
+                    newMain.addStudents(student);
+                }
+
+                newMain.sortStudentsByAge();
+                newMain.printStudents();
+            }
         }
-    }
